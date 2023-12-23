ceac006c8841fb144c7160c0fbd95665aab607ac
Sparsh Chauhan <142965846+Sparsh261@users.noreply.github.com>
2023-12-23 16:27:52 +0530

    Completed

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 let App_ID = "2abd61194f4a40d592a7bd0ef6e1e553"
 
-
 let token = null;
 let uid = String(Math.floor(Math.random()*10000));
-
 
 let client;
 let channel;
 
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let roomId = urlParams.get('room');
+
+if(!roomId){
+    window.location = 'lobby.html'
+}
 
 let local;
 let remote;
@@ -28,18 +33,18 @@ let init = async ()=>{
         await client.login({uid,token});
 
 
-        channel = client.createChannel('main');
+        channel = client.createChannel(roomId);
         await channel.join();
 
         channel.on("MemberJoined",handleUserJoined);
         channel.on("MemberLeft",handleUserLeft);
         
         
-        channel.on("MessageFromPeer",handleMessageFromPeer);
+        client.on("MessageFromPeer",handleMessageFromPeer);
 
 
 
-    local = await navigator.mediaDevices.getUserMedia({video:true,audio:false});
+    local = await navigator.mediaDevices.getUserMedia({video:true,audio:true});
     document.getElementById("user1").srcObject = local;
 
 }
@@ -48,6 +53,8 @@ let init = async ()=>{
 
 let handleUserLeft = (MemberId) => {
     document.getElementById('user2').style.display = 'none';
+
+    document.getElementById("user1").classList.remove("frame");
 }
 
 
@@ -93,6 +100,7 @@ let createPeerConnection = async (MemberId) => {
     document.getElementById("user2").srcObject = remote;
     document.getElementById("user2").style.display = 'block';
     
+    document.getElementById("user1").classList.add("frame");
 
     if(!local){
         local = await navigator.mediaDevices.getUserMedia({video:true,audio:false});
@@ -158,12 +166,41 @@ let leaveChannel = async () => {
 }
 
 
-window.addEventListener('beforeunlad',leaveChannel);
+let toggleCamera = async () => {
+    let videoTrack = local.getTracks().find(track => track.kind === 'video');
+
+    if(videoTrack.enabled){
+        videoTrack.enabled = false;
+        document.getElementById("camera-btn").style.backgroundColor = "red";  
+    }
+    else{
+        videoTrack.enabled = true;
+        document.getElementById("camera-btn").style.backgroundColor = "rgb(179,102,249)";  
+    }
+}
 
 
+
+let toggleMic = async () => {
+    let audioTrack = local.getTracks().find(track => track.kind === 'audio');
+
+    if(audioTrack.enabled){
+        audioTrack.enabled = false;
+        document.getElementById("mic-btn").style.backgroundColor = "red";  
+    }
+    else{
+        audioTrack.enabled = true;
+        document.getElementById("mic-btn").style.backgroundColor = "rgb(179,102,249,.9)";  
+    }
+}
+
+window.addEventListener('beforeunload',leaveChannel);
+
+document.getElementById("camera-btn").addEventListener('click',toggleCamera)
+
+document.getElementById("mic-btn").addEventListener('click',toggleMic)
 
 init();
-
 
 
 
